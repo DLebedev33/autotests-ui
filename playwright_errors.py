8c3c8649904686701db65993f7d46d34f39dd4ad
Ваше имя пользователя на GitHub <skvip8492@gmail.com>
2025-10-02 00:26:25 +0300

    Практика работы с авторизацией в Playwright

--- playwright_errors.py
+++ playwright_errors.py
@@ -1,6 +1,5 @@
 from playwright.sync_api import sync_playwright, expect
 
-from playwright_authorization import login_button
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
