77c1b1e0d9a5a4526f620dff1e3a7d38567e0b04
Ваше имя пользователя на GitHub <skvip8492@gmail.com>
2025-10-19 01:16:08 +0300

    Плагины Pytest

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -6,8 +6,6 @@ from playwright.sync_api import sync_playwright, Page, Playwright, expect
 def chromium_page(playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
     yield browser.new_page()
-# yield даёт нам делать какие то действия до теста и какие то действия после теста,
-# если поставить return, то браузер закроется до начала теста
     browser.close()
 
 
